5290f33c55c83b0a9a65bf61f8450f773eb2a2ca
Edouard Teulieres <edouard_teulieres@yahoo.fr>
2016-02-23 18:55:50 +0100

    implement first step

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root to: 'pages#home'
 
 
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
      resources :annonces, only: [:new, :create, :edit, :destroy ]
   end
   resources :bookings
+
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
