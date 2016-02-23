bac77225e32e013135b4ed3bc059e02af1e6d147
Guillaume Grignard <a1z2e3r4t5y67890@gmail.com>
2016-02-23 17:05:20 +0100

    full controler ok/ view edit,new ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,50 @@
+
 class BookingsController < ApplicationController
-  def booking
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if params[:booking]
+      @booking.update(bookings_params)
+    end
+      redirect_to booking_path
+  end
+
+  def create
+    @booking = Booking.new(bookings_params)
+    if @booking.save
+      flash[:notice] = "booking created!"
+      redirect_to root_path
+    else
+      flash[:blocked] = "Time period BLOCKED"
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to(root_path)
+  end
+
+private
+
+  def bookings_params
+    params.require(:booking).permit(:user_id, :annonce_id, :start_date, :end_date)
   end
 end
--- app/models/annonce.rb
+++ app/models/annonce.rb
@@ -1,5 +1,8 @@
 class Annonce < ActiveRecord::Base
   has_many :booking
   belongs_to :user
-
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
 end
