c7fdaa5e42ca0a6fd7a780956fce41d2e0f06ab0
Guillaume Grignard <a1z2e3r4t5y67890@gmail.com>
2016-02-24 12:23:53 +0100

    correction seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,35 +14,35 @@ annonces_attributes = [
     description:  "Vaste appartement en plein centre-ville de Bordeaux ",
     capacity:        4,
     price:           80
-  }
+  },
   {
     title:        "Péniche réaménagée",
     address:      "55, quai de la Reine, 75019 Paris",
     description:  "Belle péniche avec tout le confort moderne et une connexion wifi",
     capacity:        6,
     price:           250
-  }
+  },
   {
     title:        "Rooftop à New-York",
     address:      "146 E 50th St, New York, NY 10022",
     description:  "Expérience unique dans ce rooftop d'exception dominant Manatthan ",
     capacity:        10,
     price:           1000
-  }
+  },
   {
     title:        "Ferme de campagne",
     address:      "4 rue de la Chapelle, 33360 Latresne",
     description:  "Belle ferme de campagne avec une cheminée ancienne, une piscine et un étang dans le jardin",
     capacity:        20,
     price:           120
-  }
+  },
   {
     title:        "Maison avec vue sur la Mediterannée à Antibes",
     address:      "2 Boulevard Maréchal Juin, 06160 Antibes",
     description:  "Profittez de la doceur du climat dans cette maison d'architecte et sa vue imprenable",
     capacity:        8,
     price:           200
-  }
+  },
   {
     title:        "Appartement T3 situé sur la place Stanislas ",
     address:      "2 Place Stanislas, 54000 Nancy",
